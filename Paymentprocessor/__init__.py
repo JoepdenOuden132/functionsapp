179e08c5ed1b49dbdcef54103026057439e16b65
JoepdenOuden132 <jodo1327@gmail.com>
2025-01-08 03:06:24 +0100

    cgvhbj

--- Paymentprocessor/__init__.py
+++ Paymentprocessor/__init__.py
@@ -3,31 +3,6 @@ import json
 import azure.functions as func
 import pymysql
 
-def connect_to_db():
-    return pymysql.connect(
-        host='your-database-host',
-        user='your-username',
-        password='your-password',
-        database='your-database'
-    )
-
-def process_payment(event_data):
-    logging.info(f"Processing payment: {event_data}")
-
-    connection = connect_to_db()
-    try:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO payments (payment_id, amount, status, timestamp) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, (
-                event_data['payment_id'],
-                event_data['amount'],
-                event_data['status'],
-                event_data['timestamp']
-            ))
-            connection.commit()
-    finally:
-        connection.close()
-
 def main(event: func.EventGridEvent):
     logging.info('Processing event: %s', event.get_json())
     event_data = event.get_json()
